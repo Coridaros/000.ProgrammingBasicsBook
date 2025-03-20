ee130ca7048567aaf2d2188a9eefc6e6dd00e8b7
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-21 00:29:33 +0200

    Add

---
+++ 002.SimpleCalculations/006.CircleAreaAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,17 @@
+﻿//Simple calculations, Task 005
+//20.03.2025
+using System;
+
+public class CircleAreaAndPerimeter
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+
+        double area = Math.PI * r * r;
+        double perimeter = 2 * Math.PI * r;
+
+        Console.WriteLine(area);
+        Console.WriteLine(perimeter);
+    }
+}
