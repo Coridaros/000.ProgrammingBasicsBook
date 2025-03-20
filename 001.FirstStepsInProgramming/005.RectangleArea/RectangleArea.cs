bac92de9f5c0749d5c3f20377843e73ee76dfbef
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-20 21:23:09 +0200

    Add

---
+++ 001.FirstStepsInProgramming/005.RectangleArea/RectangleArea.cs
@@ -0,0 +1,16 @@
+﻿//First steps in programming, Task 005
+//19.03.2025, 23:41
+using System;
+
+public class RectangleArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+
+        double sum = a * b;
+
+        Console.WriteLine(sum);
+    }
+}
