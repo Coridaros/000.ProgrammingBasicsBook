403a0ff547477d1c56448565eb1f2436806121ce
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-21 00:28:40 +0200

    Add

---
+++ 002.SimpleCalculations/004.ConcatenateData/ConcatenateData.cs
@@ -0,0 +1,16 @@
+﻿//Simple calculations, Task 004
+//20.03.2025, 22:13
+using System;
+
+public class ConcatenateData
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string town = Console.ReadLine();
+
+        Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+    }
+}
