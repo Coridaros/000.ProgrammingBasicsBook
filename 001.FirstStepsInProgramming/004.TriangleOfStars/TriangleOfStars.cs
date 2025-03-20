ca590b5eb0a3a86f91c0062e9e3ef85ba3227bba
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-20 21:22:40 +0200

    Add

---
+++ 001.FirstStepsInProgramming/004.TriangleOfStars/TriangleOfStars.cs
@@ -0,0 +1,19 @@
+﻿//First steps in programming, Task 004
+//19.03.2025, 23:38
+using System;
+
+public class TriangleOfStars
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
