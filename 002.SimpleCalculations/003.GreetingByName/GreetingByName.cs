ea72b4ab71219d6e497221f3c277f84cc944f529
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-21 00:28:01 +0200

    Add

---
+++ 002.SimpleCalculations/003.GreetingByName/GreetingByName.cs
@@ -0,0 +1,13 @@
+﻿//Simple calculations, Task 003
+//20.03.2025, 22:09
+using System;
+
+public class GreetingByName
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
