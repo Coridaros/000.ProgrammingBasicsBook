41b99dfbd1e85fa093a63d14d0d5927bf74c0528
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-20 21:23:42 +0200

    Add

---
+++ 001.FirstStepsInProgramming/006.SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,20 @@
+﻿//First steps in programming, Task 006
+//19.03.2025, 23:46
+using System;
+
+public class SquareOfStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(new string('*', n));
+
+        for(int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine('*' + new string(' ', n - 2) + '*');
+        }
+
+        Console.WriteLine(new string('*', n));
+    }
+}
