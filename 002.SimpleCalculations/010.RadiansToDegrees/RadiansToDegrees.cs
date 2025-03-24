36cadfc861baf4596e57f881689294341df1862f
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-25 00:44:17 +0200

    add

---
+++ 002.SimpleCalculations/010.RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,15 @@
+﻿//Simple calculations, Task 010
+//20.03.2025, 23:15
+using System;
+
+public class RadiansToDegrees
+{
+    static void Main()
+    {
+        double radians = double.Parse(Console.ReadLine());
+
+        double degrees = radians * 180 / Math.PI;
+
+        Console.WriteLine(Math.Round(degrees));
+    }
+}
