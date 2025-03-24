56985e23a060bef99b656d97607ccd120d5d0952
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-25 00:43:34 +0200

    add

---
+++ 002.SimpleCalculations/008.TriangleArea/TriangleArea.cs
@@ -0,0 +1,16 @@
+﻿//Simple calculations, Task 008
+//20.03.2025, 23:02
+using System;
+
+public class TriangleArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+
+        double area = a * h / 2;
+
+        Console.WriteLine(area);
+    }
+}
