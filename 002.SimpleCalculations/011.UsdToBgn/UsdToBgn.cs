81710d90480271e5666b974bc85a09aeacf3eb4e
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-25 00:44:37 +0200

    add

---
+++ 002.SimpleCalculations/011.UsdToBgn/UsdToBgn.cs
@@ -0,0 +1,15 @@
+﻿//Simple calculation, Task 011
+//20.03.2025, 23:35
+using System;
+
+public class UsdToBgn
+{
+    static void Main()
+    {
+        double usd = double.Parse(Console.ReadLine());
+
+        double bgn = usd * 1.79549;
+
+        Console.WriteLine($"{bgn:F2} BGN");
+    }
+}
