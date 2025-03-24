5c50f2c256fd00871fc5a55d8ac7555c17de73c1
NeonCardinal <nvnnaidenov@gmail.com>
2025-03-25 00:43:57 +0200

    add

---
+++ 002.SimpleCalculations/009.CelsiusToFahrenheit/CelsiusToFahrenheit.cs
@@ -0,0 +1,15 @@
+﻿//Simple calculations, Task 009
+//20.03.2025, 23:05
+using System;
+
+public class CelsiusToFahrenheit
+{
+    static void Main()
+    {
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine(fahrenheit);
+    }
+}
