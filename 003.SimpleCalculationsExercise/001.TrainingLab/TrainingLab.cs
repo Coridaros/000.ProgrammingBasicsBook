f7bde1b3b1c11d93f8dcf2d0944efd6fb1e88d35
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-02 23:53:58 +0300

    add

---
+++ 003.SimpleCalculationsExercise/001.TrainingLab/TrainingLab.cs
@@ -0,0 +1,21 @@
+﻿//Simple calculations - exersice, Task 001
+//25.03.2025
+using System;
+
+public class TrainingLab
+{
+    static void Main()
+    {
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+
+        length *= 100;
+        width *= 100;
+
+        int cols = ((int)width - 100) / 70;
+        int rows = (int)length / 120;
+        int seat = cols * rows - 3;
+
+        Console.WriteLine(seat);
+    }
+}
