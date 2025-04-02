71ff543b30bc15054cfc7ee24d4d4b88743cd725
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-03 00:43:58 +0300

    add

---
+++ 004.SimpleConditions/002.ExcellentOrNot/ExcellentOrNot.cs
@@ -0,0 +1,20 @@
+﻿//Simple conditions, Task 002
+//03.04.2025, 00:16
+using System;
+
+public class ExcellentOrNot
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if(grade >= 5.50)
+        {
+            Console.WriteLine("Excellent!");
+        }
+        else
+        {
+            Console.WriteLine("Not excellent.");
+        }
+    }
+}
