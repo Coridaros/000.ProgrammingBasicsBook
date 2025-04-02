5f16c8ef0c8692b43252e873b919935ba7758197
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-03 00:50:58 +0300

    add

---
+++ 004.SimpleConditions/004.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,21 @@
+﻿//Simple conditions, Task 004
+//03.04.2025, 00:45
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        if(firstNumber > secondNumber)
+        {
+            Console.WriteLine(firstNumber);
+        }
+        else
+        {
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
