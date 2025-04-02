4acb6ee45575872b9b2ba0665a03bb136a5dcd7b
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-03 00:43:37 +0300

    add

---
+++ 004.SimpleConditions/001.ExcellentResult/ExcellentResult.cs
@@ -0,0 +1,16 @@
+﻿//Simple conditions, Task 001
+//03.04.2025, 00:06
+using System;
+
+public class ExcellentResult
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if(grade >= 5.50)
+        {
+            Console.WriteLine("Excellent!");
+        }
+    }
+}
