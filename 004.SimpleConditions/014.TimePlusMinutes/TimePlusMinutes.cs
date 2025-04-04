a7ae750b8cd3f448543124d80d9bc2913711943a
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-04 22:31:06 +0300

    add

---
+++ 004.SimpleConditions/014.TimePlusMinutes/TimePlusMinutes.cs
@@ -0,0 +1,34 @@
+﻿//Simple conditions, Task 014
+//04.04.2025, 22:19
+using System;
+
+public class TimePlusMinutes
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        minutes += 15;
+
+        if(minutes > 59)
+        {
+            hours++;
+            minutes -= 60;
+        }
+
+        if(hours > 23)
+        {
+            hours -= 24;
+        }
+
+        if(minutes < 10)
+        {
+            Console.WriteLine($"{hours}:0{minutes}");
+        }
+        else
+        {
+            Console.WriteLine(hours + ":" + minutes);
+        }
+    }
+}
