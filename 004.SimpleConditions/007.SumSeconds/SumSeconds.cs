4b021a20afc3129a94d5f38b84b3e2eae27b9a30
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-04 22:27:59 +0300

    add

---
+++ 004.SimpleConditions/007.SumSeconds/SumSeconds.cs
@@ -0,0 +1,38 @@
+﻿//Simple conditions, Task 007
+//04.04.2025, 21:06
+using System;
+
+public class SumSeconds
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+
+        int seconds = first + second + third;
+
+        int minutes = 0;
+
+        if(seconds > 59)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+
+        if (seconds > 59)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+
+        if(seconds < 10)
+        {
+            Console.WriteLine(minutes + ":" + "0" + seconds);
+        }
+        else
+        {
+            Console.WriteLine(minutes + ":" + seconds);
+        }
+    }
+}
