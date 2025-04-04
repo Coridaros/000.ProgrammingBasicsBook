045e56b71b5ea06b9efd13230ac47e59184c74c5
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-04 22:29:55 +0300

    add

---
+++ 004.SimpleConditions/011.EqualWords/EqualWords.cs
@@ -0,0 +1,21 @@
+﻿//Simple conditions, Task 011
+//04.04.2025, 21:50
+using System;
+
+public class EqualWords
+{
+    static void Main()
+    {
+        string firstWord = Console.ReadLine().ToLower();
+        string secondWord = Console.ReadLine().ToLower();
+
+        if(firstWord == secondWord)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
