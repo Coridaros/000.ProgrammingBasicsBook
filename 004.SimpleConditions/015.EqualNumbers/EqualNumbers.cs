ea9becd9e6e59988aa3a3c7793f4f4a804c0a0ce
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-04 22:31:35 +0300

    add

---
+++ 004.SimpleConditions/015.EqualNumbers/EqualNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Simple conditions, Task 015
+//04.04.2025, 22:24
+using System;
+
+public class EqualNumbers
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        if(firstNumber == secondNumber && firstNumber == thirdNumber)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
