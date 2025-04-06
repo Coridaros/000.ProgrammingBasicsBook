1691e75057cda8b0371aa030eb238adf61415428
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-06 19:21:14 +0300

    add

---
+++ 006.ComplexConditions/001.PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,35 @@
+﻿//Complex conditions, Task 001
+//06.04.2025, 18:31
+using System;
+
+public class PersonalTitles
+{
+    static void Main()
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+
+        if(age < 16)
+        {
+            if(gender == "m")
+            {
+                Console.WriteLine("Master");
+            }
+            else if(gender == "f")
+            {
+                Console.WriteLine("Miss");
+            }
+        }
+        else if(age >= 16)
+        {
+            if (gender == "m")
+            {
+                Console.WriteLine("Mr.");
+            }
+            else if (gender == "f")
+            {
+                Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
