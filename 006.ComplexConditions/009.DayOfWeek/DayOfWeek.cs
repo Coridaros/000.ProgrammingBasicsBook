8525534c8c66504e2274f302d46bea8f537cca2c
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-07 00:12:42 +0300

    add

---
+++ 006.ComplexConditions/009.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,39 @@
+﻿//Complex conditions, Task 009
+//06.04.2025, 23:52
+using System;
+
+public class DayOfWeek
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        switch(num)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Error!");
+                break;
+        }
+    }
+}
