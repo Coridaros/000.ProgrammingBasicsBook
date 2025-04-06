a31736b54bc93396bd72f9337a673061822e5dcf
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-06 19:43:00 +0300

    add

---
+++ 006.ComplexConditions/003.PointInRectangle/PointInRectangle.cs
@@ -0,0 +1,26 @@
+﻿//Complex conditions, Task 003
+//06.04.2025, 19:23
+using System;
+
+public class PointInRectangle
+{
+    static void Main()
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        if(x >= x1 && x <= x2 && y >= y1 && y <= y2)
+        {
+            Console.WriteLine("Inside");
+        }
+        else
+        {
+            Console.WriteLine("Outside");
+        }
+
+    }
+}
