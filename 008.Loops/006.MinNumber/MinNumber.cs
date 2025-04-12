5db08b275c49ae35d58e6f5c33dfaa62def51d00
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-12 16:33:02 +0300

    add

---
+++ 008.Loops/006.MinNumber/MinNumber.cs
@@ -0,0 +1,25 @@
+﻿//Loops, Task 006
+//12.04.2025, 16:21
+using System;
+
+public class MinNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int minNumber = int.MaxValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number < minNumber)
+            {
+                minNumber = number;
+            }
+        }
+
+        Console.WriteLine(minNumber);
+    }
+}
