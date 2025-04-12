4cba7d6ddde610a8a2806771b6f7f0f55f31ae01
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-12 16:30:49 +0300

    add

---
+++ 008.Loops/001.NumbersOneToOneHundred/NumbersOneToOneHundred.cs
@@ -0,0 +1,14 @@
+﻿//Loops, Task 001
+//12.04.2025, 13:55
+using System;
+
+public class NumbersOneToOneHundred
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
