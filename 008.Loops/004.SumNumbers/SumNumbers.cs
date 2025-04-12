04577848c5114f5e4e3317720cfb0c449669720e
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-12 16:32:18 +0300

    add

---
+++ 008.Loops/004.SumNumbers/SumNumbers.cs
@@ -0,0 +1,20 @@
+﻿//Loops, Task 004
+//12.04.2025, 14:30
+using System;
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            sum += int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
