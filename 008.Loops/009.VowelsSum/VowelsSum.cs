70602620b7e15525e099b9f164775a7c27045473
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-13 00:49:52 +0300

    add

---
+++ 008.Loops/009.VowelsSum/VowelsSum.cs
@@ -0,0 +1,39 @@
+﻿//Loops, Task 009
+//12.04.2025, 19:30
+using System;
+
+public class VowelsSum
+{
+    static void Main()
+    {
+        string msg = Console.ReadLine();
+
+        int sum = 0;
+
+        for(int i = 0; i < msg.Length; i++)
+        {
+            if (msg[i] == 'a')
+            {
+                sum += 1;
+            }
+            else if (msg[i] == 'e')
+            {
+                sum += 2;
+            }
+            else if (msg[i] == 'i')
+            {
+                sum += 3;
+            }
+            else if (msg[i] == 'o')
+            {
+                sum += 4;
+            }
+            else if (msg[i] == 'u')
+            {
+                sum += 5;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
