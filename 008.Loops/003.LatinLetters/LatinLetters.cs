f0ce1a25b4a822c58a9e395661b25f3124a76199
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-12 16:31:55 +0300

    add

---
+++ 008.Loops/003.LatinLetters/LatinLetters.cs
@@ -0,0 +1,14 @@
+﻿//Loops, Task 003
+//12.04.2025, 14:01
+using System;
+
+public class LatinLetters
+{
+    static void Main()
+    {
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+            Console.WriteLine(letter);
+        }
+    }
+}
