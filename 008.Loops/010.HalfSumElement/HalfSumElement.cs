32c030860ddadcba0e3a629a2efb8b7ead74b29b
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-13 00:50:14 +0300

    add

---
+++ 008.Loops/010.HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,36 @@
+﻿//Loops, Task 010
+//12.04.2025, 19:37
+using System;
+
+public class HalfSumElement
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int diff = 0;
+        int max = int.MinValue;
+
+        for (int i = 0; i < inputNumber; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+            sum += currentNumber;
+
+            if (currentNumber > max)
+            {
+                max = currentNumber;
+            }
+
+            diff = Math.Abs((max * 2) - sum);
+        }
+
+        if (max == (sum - max))
+        {
+            Console.WriteLine("Yes sum = " + max);
+        }
+        else
+        {
+            Console.WriteLine("No diff = " + diff);
+        }
+    }
+}
