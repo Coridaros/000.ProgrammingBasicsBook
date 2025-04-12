6521b94706d931b66ce6beba313ca947eaeba7f6
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-13 00:49:11 +0300

    add

---
+++ 008.Loops/007.LeftAndRightSum/LeftAndRightSum.cs
@@ -0,0 +1,32 @@
+﻿//Loops, Task 007
+//12.04.2025, 19:17
+using System;
+
+public class LeftAndRightSum
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            leftSum += int.Parse(Console.ReadLine());
+        }
+        for(int i = 0; i < n; i++)
+        {
+            rightSum += int.Parse(Console.ReadLine());
+        }
+
+        if(leftSum == rightSum)
+        {
+            Console.WriteLine($"Yes, sum = {leftSum}");
+        }
+        else
+        {
+            Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+        }
+    }
+}
