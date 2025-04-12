609e98fc3515092bcbd61c4abd3b0a9b808cd6ed
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-12 16:31:24 +0300

    add

---
+++ 008.Loops/002.NumbersEndingInSeven/NumbersEndingInSeven.cs
@@ -0,0 +1,17 @@
+﻿//Loops, Task 002
+//12.04.2025, 13:58
+using System;
+
+public class NumbersEndingInSeven
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 1000; i++)
+        {
+            if(i % 10 == 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
