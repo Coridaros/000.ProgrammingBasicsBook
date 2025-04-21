633b6aa03ac1e37b0590eca4d78c0e7eea5695c0
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-22 01:33:18 +0300

    add

---
+++ 010.NestedLoops/004.TriangleOfDollars/Program.cs
@@ -0,0 +1,23 @@
+﻿//Nested loops, Task 004
+//22.04.2025, 01:27
+using System;
+
+public class TriangleOfDollars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            Console.Write("$");
+
+            for(int j = 1; j < i; j++)
+            {
+                Console.Write(" $");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
