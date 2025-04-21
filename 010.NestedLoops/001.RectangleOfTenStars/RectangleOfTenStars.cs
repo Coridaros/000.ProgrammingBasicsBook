4239accee67c4904bd7aa7d6b9969268b9acb22c
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-22 01:32:19 +0300

    add

---
+++ 010.NestedLoops/001.RectangleOfTenStars/RectangleOfTenStars.cs
@@ -0,0 +1,14 @@
+﻿//Nested loops, Task 001
+//18.04.2025, 01:13
+using System;
+
+public class RectangleOfTenStars
+{
+    static void Main()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(new string('*', 10));
+        }
+    }
+}
