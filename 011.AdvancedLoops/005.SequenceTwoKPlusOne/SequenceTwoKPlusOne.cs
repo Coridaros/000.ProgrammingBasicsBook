ecfea6fcaef0bd94b8269de74ff57ff5cbcc7496
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-23 00:03:43 +0300

    add

---
+++ 011.AdvancedLoops/005.SequenceTwoKPlusOne/SequenceTwoKPlusOne.cs
@@ -0,0 +1,20 @@
+﻿//Advanced loops, Task 005
+//22.04.2025, 01:57
+using System;
+
+public class SequenceTwoKPlusOne
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int number = 1;
+
+        while(number <= n)
+        {
+            Console.WriteLine(number);
+
+            number = 2 * number + 1;
+        }
+    }
+}
