73877ec951b95f42f547b5cfd17cacf723428799
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-23 00:02:38 +0300

    add

---
+++ 011.AdvancedLoops/002.NumbersNToOne/NumbersNToOne.cs
@@ -0,0 +1,16 @@
+﻿//Advanced loops, Task 002
+//22.04.2025, 01:40
+using System;
+
+public class NumbersNToOne
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = n; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
