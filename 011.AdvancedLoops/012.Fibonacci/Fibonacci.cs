7d46ba091f57d74ae0cacbd35c924216ca53169c
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:05:27 +0300

    add

---
+++ 011.AdvancedLoops/012.Fibonacci/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿//Advanced loops, Task 012
+//22.04.2025, 21:41
+using System;
+
+public class Fibonacci
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int firstF = 1;
+        int secondF = 1;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int nextF = firstF + secondF;
+            firstF = secondF;
+            secondF = nextF;
+        }
+
+        Console.WriteLine(secondF);
+    }
+}
