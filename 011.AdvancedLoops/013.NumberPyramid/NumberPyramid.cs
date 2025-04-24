43b06ca3c5598d013a0ddf070e50a5ee69b3caf8
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:06:42 +0300

    add

---
+++ 011.AdvancedLoops/013.NumberPyramid/NumberPyramid.cs
@@ -0,0 +1,40 @@
+﻿//Advanced loops, Task 013
+//22.04.2025, 21:46
+using System;
+
+public class NumberPyramid
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int num = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                if(j > 1)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(num);
+
+                num++;
+
+                if(num > n)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            if(num > n)
+            {
+                break;
+            }
+        }
+    }
+}
