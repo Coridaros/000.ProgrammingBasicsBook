4c442705c6f7f3b06b8cd6b89862c1fd068f9d21
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:03:11 +0300

    адд

---
+++ 011.AdvancedLoops/008.Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿//Advanced loops, Task 008
+//22.04.2025, 02:21
+using System;
+
+public class Factorial
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int fact = 1;
+
+        do
+        {
+            fact = fact * n;
+            n--;
+        }
+        while (n > 1);
+
+        Console.WriteLine(fact);
+    }
+}
