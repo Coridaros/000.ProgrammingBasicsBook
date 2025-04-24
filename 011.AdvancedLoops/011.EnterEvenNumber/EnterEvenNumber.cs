bc880972a0a8810b48f0a50445762ded1c624a7a
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:04:20 +0300

    add

---
+++ 011.AdvancedLoops/011.EnterEvenNumber/EnterEvenNumber.cs
@@ -0,0 +1,31 @@
+﻿//Advanced looops, Task 011
+//22.04.2025, 21:30
+using System;
+
+public class EnterEvenNumber
+{
+    static void Main()
+    {
+        while(true)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if(n % 2 == 0)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+
+                Console.WriteLine("The number is not even.");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number.");
+            }
+        }
+
+
+    }
+}
