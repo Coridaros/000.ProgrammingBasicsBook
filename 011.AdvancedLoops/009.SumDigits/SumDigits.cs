14697b62428132b0a6902bd0488009385687c01e
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:03:34 +0300

    адд

---
+++ 011.AdvancedLoops/009.SumDigits/SumDigits.cs
@@ -0,0 +1,22 @@
+﻿//Advanced loops, Task 009
+//22.04.2025, 02:31
+using System;
+
+public class SumDigits
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        do
+        {
+            sum = sum + (n % 10);
+            n = n / 10;
+        }
+        while (n > 0);
+
+        Console.WriteLine(sum);
+    }
+}
