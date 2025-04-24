795ab7725a98dfaab5495a135a225f2f9ccc5905
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-24 22:04:00 +0300

    add

---
+++ 011.AdvancedLoops/010.CheckPrime/CheckPrime.cs
@@ -0,0 +1,31 @@
+﻿//Advanced loops, Task 010
+//22.04.2025, 02:39
+using System;
+
+public class CheckPrime
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool prime = true;
+
+        for(int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if(n % i == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime)
+        {
+            Console.WriteLine("Prime");
+        }
+        else
+        {
+            Console.WriteLine("Not prime");
+        }
+    }
+}
