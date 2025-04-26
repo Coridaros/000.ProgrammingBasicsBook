2e9759e860d29a24768a8c52f731907b75b8a7e8
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-27 00:58:33 +0300

    add

---
+++ 013.ExamPreparationParOne/001.TriangleArea/TriangleArea.cs
@@ -0,0 +1,23 @@
+﻿//Exam preparation part 1, Task 001
+//23.04.2025, 23:04
+using System;
+
+public class TriangleArea
+{
+    static void Main()
+    {
+        int x1 = int.Parse(Console.ReadLine());
+        int y1 = int.Parse(Console.ReadLine());
+        int x2 = int.Parse(Console.ReadLine());
+        int y2 = int.Parse(Console.ReadLine());
+        int x3 = int.Parse(Console.ReadLine());
+        int y3 = int.Parse(Console.ReadLine());
+
+        int a = Math.Abs(x2 - x3);
+        int b = Math.Abs(y2 - y1);
+
+        double area = (double)a * b / 2;
+
+        Console.WriteLine(area);
+    }
+}
