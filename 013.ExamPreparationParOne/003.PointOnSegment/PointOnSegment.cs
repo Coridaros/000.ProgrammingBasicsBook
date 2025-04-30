55bdf5f96f5badf23db876167d6542797f8e9494
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-30 22:56:39 +0300

    add

---
+++ 013.ExamPreparationParOne/003.PointOnSegment/PointOnSegment.cs
@@ -0,0 +1,32 @@
+﻿//Exam preparation part 1, Task 003
+//30.04.2025, 22:09
+using System;
+
+public class PointOnSegment
+{
+    static void Main()
+    {
+        int startX = int.Parse(Console.ReadLine());
+        int endX = int.Parse(Console.ReadLine());
+        int x = int.Parse(Console.ReadLine());
+
+        int left = Math.Min(startX, endX);
+        int right = Math.Max(startX, endX);
+
+        int distanceLeft = Math.Abs(left - x);
+        int distanceRight = Math.Abs(right - x);
+
+        int result = Math.Min(distanceLeft, distanceRight);
+
+        if(x >= left && x <= right)
+        {
+            Console.WriteLine("in");
+        }
+        else
+        {
+            Console.WriteLine("out");
+        }
+
+        Console.WriteLine(result);
+    }
+}
