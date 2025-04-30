1406a68c93255b54e6ad465a448f2b8df1cfe79a
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-30 23:06:41 +0300

    add

---
+++ 013.ExamPreparationParOne/004.PointInFigure/PointInFigure.cs
@@ -0,0 +1,24 @@
+﻿//Exam preparation part 1, Task 004
+//30.04.2025, 22:58
+using System;
+
+public class PointInFigure
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        bool rectOne = x >= 2 && x <= 12 && y >= -3 && y <= 1;
+        bool rectTwo = x >= 4 && x <= 10 && y >= -5 && y <= 3;
+
+        if(rectOne || rectTwo)
+        {
+            Console.WriteLine("in");
+        }
+        else
+        {
+            Console.WriteLine("out");
+        }
+    }
+}
