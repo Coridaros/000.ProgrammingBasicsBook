5e56ae9def361b3a2c389c00477d72b927457dd6
NeonCardinal <nvnnaidenov@gmail.com>
2025-04-30 22:56:10 +0300

    add

---
+++ 013.ExamPreparationParOne/002.Bricks/Bricks.cs
@@ -0,0 +1,17 @@
+﻿//Exam preparation part 1, Task 002
+//30.04.2025, 22:00
+using System;
+
+public class Bricks
+{
+    static void Main()
+    {
+        int bricksCnt = int.Parse(Console.ReadLine());
+        int workersCnt = int.Parse(Console.ReadLine());
+        int capacity = int.Parse(Console.ReadLine());
+
+        double courses = Math.Ceiling((double)bricksCnt / (workersCnt * capacity));
+
+        Console.WriteLine(courses);
+    }
+}
